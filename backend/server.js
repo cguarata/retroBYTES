a1bec67a47556b330f0fd56a0c3d01c77a9fb4cf
cguarata <cguarata@gmail.com>
2021-08-26 02:42:14 +0200

    first commit

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,11 @@ const express = require("express");
 const morgan = require("morgan");
 
 const { PORT, HOST } = process.env;
-// creo app express
+
 const app = express();
 
-// uso middleware morgan
 app.use(morgan("dev"));
 
-// middleware que parsea el body
 app.use(express.json());
 
 //middleware que captura todas las rutas
